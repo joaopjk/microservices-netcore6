9b2ad0853dd021db13c6f79e6438254c62826b5a
joaopjk <joaopjk48@gmail.com>
2021-09-04 15:39:40 -0300

    Configuring DbContext methods

---
+++ MangoRestaurant/Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
--- MangoRestaurant/Mango.Services.ProductAPI/Startup.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Mango.Services.ProductAPI.DbContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ProductAPI
 {
@@ -26,7 +21,8 @@ namespace Mango.Services.ProductAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
