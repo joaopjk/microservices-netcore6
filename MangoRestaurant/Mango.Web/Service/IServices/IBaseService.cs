cb7635d3da42870b04e71163daef8dc067527e98
joaopjk <joaopjk48@gmail.com>
2021-09-04 20:05:35 -0300

    Base service interface and API Request Model

---
+++ MangoRestaurant/Mango.Web/Service/IServices/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mango.Web.Models;
+
+namespace Mango.Web.Service.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDTO responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
---
+++ MangoRestaurant/Mango.Web/Service/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Mango.Web.Models;
+
+namespace Mango.Web.Service.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDTO productDto);
+        Task<T> UpdateProductAsync<T>(ProductDTO productDto);
+        Task<T> DeleteProductAsync<T>(int id);
+    }
+}
