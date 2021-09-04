c6fb190f763df1d67eb91edfba83fc433ee3e863
joaopjk <joaopjk48@gmail.com>
2021-09-04 18:10:00 -0300

    Implementation Products Controller (Gets)

---
+++ MangoRestaurant/Mango.Services.ProductAPI/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mango.Services.ProductAPI.Model.DTO;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        protected ResponseDTO _response;
+        private IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _response = new ResponseDTO();
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ResponseDTO> Get()
+        {
+            try
+            {
+                _response.Result = await _productRepository.GetProducts();
+
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ResponseDTO> GetById(int id)
+        {
+            try
+            {
+                _response.Result = await _productRepository.GetProductById(id);
+
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
+        } 
+    }
+}
