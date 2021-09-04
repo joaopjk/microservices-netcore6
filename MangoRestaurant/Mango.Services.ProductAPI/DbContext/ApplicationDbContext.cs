843cc793bab65b7730988e560ae9dcabb0972dcb
joaopjk <joaopjk48@gmail.com>
2021-09-04 15:46:34 -0300

    Create and update migration for Products model

--- MangoRestaurant/Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.ProductAPI.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ProductAPI.DbContext
 {
     public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
     }
 }
