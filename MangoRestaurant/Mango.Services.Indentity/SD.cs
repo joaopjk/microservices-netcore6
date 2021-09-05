edaf6f2f5731758da66255f0d4557c46ed8d1f1d
joaopjk <joaopjk48@gmail.com>
2021-09-04 23:27:40 -0300

    Configuring Clients if Identity Server

---
+++ MangoRestaurant/Mango.Services.Indentity/SD.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace Mango.Services.Indentity
+{
+    public static class SD
+    {
+        public const string Admin = "Admin";
+        public const string Costumer = "Costumer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("Mango", "Mango Server"),
+                new ApiScope(name: "read",   displayName: "Read your data."),
+                new ApiScope(name: "write",  displayName: "Write your data."),
+                new ApiScope(name: "delete", displayName: "Delete your data.")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"read", "write", "profile"}
+                },
+                new Client
+                {
+                    ClientId = "mango",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = {"http://localhost:44373/signin-oidc"},
+                    PostLogoutRedirectUris = {"http://localhost:44373/signin-callback-oidc"},
+                    AllowedScopes = new List<string>()
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "mango"
+                    }
+                }
+
+            };
+    }
+}
