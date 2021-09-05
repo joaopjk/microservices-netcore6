cbb058090b134957b948e76a8e61b37c5642ce77
joaopjk <joaopjk48@gmail.com>
2021-09-04 23:03:27 -0300

    Create DbContext for IdentityUser

---
+++ MangoRestaurant/Mango.Services.Indentity/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Services.Indentity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.Indentity.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
