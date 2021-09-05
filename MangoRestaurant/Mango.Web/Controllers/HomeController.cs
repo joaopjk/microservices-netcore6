0d3a1908e23912779f52466b7e0ee38b3c19ab4b
joaopjk <joaopjk48@gmail.com>
2021-09-05 15:02:23 -0300

    Create index action methdo

--- MangoRestaurant/Mango.Web/Controllers/HomeController.cs
+++ MangoRestaurant/Mango.Web/Controllers/HomeController.cs
@@ -1,23 +1,38 @@
-﻿using Mango.Web.Models;
+﻿using System;
+using System.Collections.Generic;
+using Mango.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using Mango.Web.Service.IServices;
 using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IProductService _productService;
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDTO> list = new();
+
+            var response = await _productService.GetAllProductsAsync<ResponseDTO>();
+            if (response is { IsSuccess: true })
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDTO>>(
+                    Convert.ToString(response.Result) ?? string.Empty);
+            }
+
+            return View(list);
         }
 
         public IActionResult Privacy()
